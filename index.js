64ca323925ee40f4c6120772d8bea8a6e5388c7c
Mark <mark.j.wee@gmail.com>
2022-01-05 14:17:19 -0500

    pushed for troubleshooting

--- index.js
+++ index.js
@@ -11,6 +11,37 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased(array) {
+  let updatedArray = []
+  updatedArray = array.map(titleCaseString())
+  return updatedArray;
 }
+
+function titleCaseString(string) {
+  let titleCasedString = ""
+  let words = string.split(" ");
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i][0].toUpperCase() + words[i].substr(1);
+  }
+  titleCasedString = words.join(" ");
+  return titleCasedString;
+}
+
+// const titleCased = (array) => {
+//   array.map(word => ({
+//     for (const i in word) {
+//       word[i].toUpperCase()
+//     })
+// }
+//   return array
+// }
+
+function titleCased(arrayOfStrings) {
+  let titleCasedString = [...arrayOfStrings]
+  for (const string of array => (array.toLowerCase().split(" ").map(function (string) {
+      return string[0].toUpperCase() + word.substring(1).join(" ");
+    })))
+  return titleCasedString
+}
+
+myStr.toLowerCase().split(' ').map(word => word.charAt(0).toUpperCase() + word.substring(1)).join(' ');

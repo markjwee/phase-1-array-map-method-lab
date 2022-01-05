7c517bbedbf6a378da7f374488bc89d569c58ea3
Mark <mark.j.wee@gmail.com>
2022-01-05 15:09:59 -0500

    Done.

--- index.js
+++ index.js
@@ -1,29 +1,7 @@
-const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
-];
-
 function titleCased(array) {
-  let titleCasedArray = []
-  titleCasedArray = array.map(titleCaseString(element) => {
-    Object.assign({}, array)
-  });
+  const titleCasedArray = array.map(titleCaseString)
   return titleCasedArray;
 }
-
-// function titleCased(array) {
-//   let updatedArray = []
-//   updatedArray = map(titleCaseString(array))
-//   return updatedArray;
-// }
 
 function titleCaseString(string) {
   let titleCasedString = ""
@@ -34,22 +12,3 @@ function titleCaseString(string) {
   titleCasedString = words.join(" ");
   return titleCasedString;
 }
-
-// const titleCased = (array) => {
-//   array.map(word => ({
-//     for (const i in word) {
-//       word[i].toUpperCase()
-//     })
-// }
-//   return array
-// }
-
-function titleCased(arrayOfStrings) {
-  let titleCasedString = [...arrayOfStrings]
-  for (const string of array => (array.toLowerCase().split(" ").map(function (string) {
-      return string[0].toUpperCase() + word.substring(1).join(" ");
-    })))
-  return titleCasedString
-}
-
-myStr.toLowerCase().split(' ').map(word => word.charAt(0).toUpperCase() + word.substring(1)).join(' ');
